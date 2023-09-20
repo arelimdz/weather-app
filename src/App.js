b4c9fb481666f1a84e0eb324dcc81923b2d62687
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-20 19:19:14 +1000

    feat: button modify daysCount

--- src/App.js
+++ src/App.js
@@ -15,8 +15,22 @@ class App extends React.Component{
 
     //  start with at least current weather and one day after
     this.state = {
-      daysCount : 1,
+      dayCount : 1,
       city: 'cityName'
+    }
+  }
+
+  decreaseDayCount(currentCount){
+    if (currentCount > 0){
+      this.setState({dayCount: this.state.dayCount - 1});
+    }
+  }
+
+  addDayCount(currentCount){
+    if (currentCount <= 10 ){
+      this.setState({dayCount: this.state.dayCount + 1});
+    } else {
+      console.log('Max forecast days reached');
     }
   }
 
@@ -26,6 +40,9 @@ class App extends React.Component{
         <h1>Weather app</h1>
         <h3>The current weather in Melbourne is:</h3>
         <Weather/>
+
+        <button onClick={() =>{this.addDayCount(this.state.dayCount)}}>Add day</button>
+        <button onClick={() =>{this.decreaseDayCount(this.state.dayCount)}}>Remove day</button>
 
         <h3>The weather tomorro will be</h3>
       </div>
