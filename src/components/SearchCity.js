fd37f4cf2dc89eea1e14b4d34b6a7cde357efe65
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-27 21:52:00 +1000

    feat: change SearchCity component to function hook

--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -1,40 +1,79 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import { useState } from "react";
 
-class SearchCity extends Component{
-    constructor(props){
-        super(props);
 
-        this.state = {
-            city: '',
-        };
-    }
+export default function SearchCity({ onCityInputChange }){
 
-    handleCityChange =(event) =>{
-        this.setState({city : event.target.value});
+    const [city, setCity] = useState('');
+
+    const handleSubmit =(event)=>{
+        event.preventDefault();
     };
 
-    handleSubmit = (event) => {
-        event.preventDefault(); // Prevent the form from submitting
-        // Pass the selected city to the parent component for fetching weather data
-        this.props.onCitySubmit(this.state.city);
+    const handleChangeCity = (event) => {
+        const newCity = event.target.value;
+        setCity(newCity);
+
+        // Call the callback function to pass the value to the parent
+        onCityInputChange(newCity);
     };
-    
-      
-    render() {
-        return (
-            <div>
-            <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                placeholder="Enter city"
-                value={this.state.city}
-                onChange={this.handleCityChange}
-              />
-              <button type="submit">Search</button>
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    name='city'
+                    placeholder='Enter City'
+                    value={city}
+                    onChange={handleChangeCity}/>
+                    <button className="searchButton" type="submit">Search</button>
             </form>
-          </div>
-        );
-    }
+        </div>
+    );
 }
 
-export default SearchCity;
+
+
+
+
+
+
+// class SearchCity extends Component{
+//     constructor(props){
+//         super(props);
+
+//         this.state = {
+//             city: '',
+//         };
+//     }
+
+//     handleCityChange =(event) =>{
+//         this.setState({city : event.target.value});
+//     };
+
+//     handleSubmit = (event) => {
+//         event.preventDefault(); // Prevent the form from submitting
+//         // Pass the selected city to the parent component for fetching weather data
+//         this.props.onCitySubmit(this.state.city);
+//     };
+    
+      
+//     render() {
+//         return (
+//             <div>
+//             <form onSubmit={this.handleSubmit}>
+//               <input
+//                 type="text"
+//                 placeholder="Enter city"
+//                 value={this.state.city}
+//                 onChange={this.handleCityChange}
+//               />
+//               <button className="searchButton" type="submit">Search</button>
+//             </form>
+//           </div>
+//         );
+//     }
+// }
+
+// export default SearchCity;
