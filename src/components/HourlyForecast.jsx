476060614d492f57e837d83252d8e482f3e3587b
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-05 19:03:07 +1100

    feat: Display only weather forecast hourly for a day

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import formatDateTime from "../utils/formatLocalTime";
 
-export default function  HourlyForecast ({ data }){
+export default function HourlyForecast ({ data }){
 
     if (!data) {
         return null;
@@ -9,25 +9,38 @@ export default function  HourlyForecast ({ data }){
 
 
     return (
-        <div>
-            <h2>Hourly forecast</h2>
-            <div>
-                {data.forecast.forecastday[0].hour.map((hour, index) => (
-                    <div key={index}>
-                        <p>{formatDateTime("short-time", hour.time )}</p>
-                        <p>{hour.temp_c} °C</p>
-                        <img src={hour.condition.icon} alt="api icon" />
-                        {
-                            hour.chance_of_rain >= 1 &&
-                            (
-                                <p>Raining: {hour.chance_of_rain}%</p> 
-                            )
-                        }
-                        <p>condition text: {hour.condition.text}</p>
+      <div className="card-hforecast">
+        <p>{formatDateTime("short-time", data.time )}</p>
+                     <p>{data.temp_c} °C</p>
+                     <img src={data.condition.icon} alt="api icon" />
+                     {
+                        data.chance_of_rain >= 1 &&
+                         (
+                             <p>Raining: {data.chance_of_rain}%</p> 
+                         )
+                     }
+                     <p>condition text: {data.condition.text}</p>
 
-                    </div>
-                ))}
-            </div>
-        </div>
+      </div>  
     )
+    // return (
+    //     <div className="hforecast_card">
+
+    //         {data.forecast.forecastday[0].hour.map((hour, index) => (
+    //             <div key={index}>
+    //                 <p>{formatDateTime("short-time", hour.time )}</p>
+    //                 <p>{hour.temp_c} °C</p>
+    //                 <img src={hour.condition.icon} alt="api icon" />
+    //                 {
+    //                     hour.chance_of_rain >= 1 &&
+    //                     (
+    //                         <p>Raining: {hour.chance_of_rain}%</p> 
+    //                     )
+    //                 }
+    //                 <p>condition text: {hour.condition.text}</p>
+
+    //             </div>
+    //         ))}
+    //     </div>
+    // )
 }
--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -6,7 +6,15 @@ export default function WeatherDisplay({ ApiData }) {
     if (!ApiData) {
       return null
     }
+    const timeIndexStart = ()=>{
+        const date = new Date(ApiData.location.localtime);
+        const hours = date.getHours(); 
+        return hours
+    }
 
+    const allItems = ApiData.forecast.forecastday[0].hour
+    const startingIndex = timeIndexStart()
+    const items = allItems.slice(startingIndex, allItems.length)
 
     return (
       <div>
@@ -14,10 +22,22 @@ export default function WeatherDisplay({ ApiData }) {
         <h2>{ApiData.current.condition.text}</h2>
         <h1>{ApiData.current.temp_c}°C</h1>
         <h3>{formatDateTime("date-time",ApiData.location.localtime)}</h3>
+        <p>{timeIndexStart()}</p>
         <div className="hforecast">
-          <HourlyForecast data={ApiData}/>
-        </div>
+          <h2>Hourly forecast</h2>
 
+          {items.map((item) => (
+              <HourlyForecast key={item.id} data={item} />
+          ))}
+
+
+          {/* {ApiData.forecast.forecastday[0].hour.map((item) => (
+              <HourlyForecast key={item.id} data={item} />
+          ))} */}
+
+          {/* <HourlyForecast data={ApiData}/> */}
+        </div>
+        
         <DailyForecast weatherData={ApiData} />
       </div>
     );
