f3581d8f94d864aae9b99ce33af396380f33e6b1
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-06 14:03:55 +1100

    feat: Convert HomePage for Clouds components

---
+++ src/pages/Cluds.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../styles/HomePage.css";
+
+export default function Clouds(){
+
+	return(	
+		<div classNAme="clouds_conatiner">
+			<div className="cloud x1"></div>
+			<div className="cloud x2"></div>
+			<div className="cloud x3"></div>
+			<div className="cloud x4"></div>
+			<div className="cloud x5"></div>
+
+			
+		</div>		
+	)
+}
